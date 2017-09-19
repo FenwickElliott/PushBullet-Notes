c6782e607b6ebf9d5925b36ef0e2800fa80289e8
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-19 15:42:49 -0400

    openDatabase running without errors or created db

--- dry.js
+++ dry.js
@@ -10,3 +10,7 @@ butt.onclick = function(){
     console.log('butt');
     hippo.innerHTML = "I hate raccons!"
 }
+
+var db = openDatabase('dryDB1', '1.0', 'dry run database', 2*1024*1024);
+
+console.log(db);
