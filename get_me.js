e3f76f939c63f2eeb96232f99a04b864d0907246
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-20 11:58:38 -0400

    fetch refactored

--- get_me.js
+++ get_me.js
@@ -1,17 +1,9 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const xhr = new XMLHttpRequest();
-
 const Secrets = require('./secrets')
 const url = 'https://api.pushbullet.com/v2/users/me';
 
-xhr.open('GET', url);
-xhr.responseType = 'json';
-xhr.setRequestHeader("Access-Token", Secrets.api_key);
-xhr.onreadystatechange = function () {
-    if(xhr.readyState === XMLHttpRequest.DONE) {
-        console.log(xhr.responseText);
-    }
-};
-xhr.send();
+const fetch = require('node-fetch');
 
-console.log(xhr.status);
+
+fetch('http://jsonplaceholder.typicode.com/posts/1')
+.then(res => res.json())
+.then(json => console.log(json));
