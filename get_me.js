3554d895116cba4cf5085d495ca75b0c729f8d97
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-20 12:20:53 -0400

    get_me writing to JSON file

--- get_me.js
+++ get_me.js
@@ -1,11 +1,15 @@
 const Secrets = require('./secrets')
 const url = 'https://api.pushbullet.com/v2/users/me';
-
+const jsonfile = require('jsonfile')
 const fetch = require('node-fetch');
 
-
-fetch(url, {
+var me = fetch(url, {
     headers: {"Access-Token": Secrets.api_key}
 })
 .then(res => res.json())
-.then(json => console.log(json));
+.then(function(json){
+    jsonfile.writeFile('./me.json', json, function (err) {
+        console.error(err)
+       })
+    return;
+});
